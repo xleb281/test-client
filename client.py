181b4d3d3d021596dae0c3f33b2ecf3189f31c47
xleb281 <160697847+xleb281@users.noreply.github.com>
2024-03-10 10:40:20 +0600

    Add files via upload

---
+++ client.py
@@ -0,0 +1,42 @@
+import socket
+import time
+import platform
+import psutil
+import os 
+import shutil
+
+
+server_address = ('127.0.0.1', 5555) # укажите адрес и порт сервера
+processes = psutil.process_iter()
+connected = False
+while not connected:
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(server_address)
+        connected = True
+        print('Connected to server')
+    except ConnectionRefusedError:
+        print('Connection failed, retrying in 5 seconds...')
+        time.sleep(5)
+
+while True:
+    data = client.recv(1024).decode()
+    
+    if data == "stop":
+        message = "stop"
+        client.send(message.encode())
+        exit()
+    elif data == "info":
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+        message = "IP:" + ip_address + ";  name:" + socket.gethostname() + ";  system version:" + platform.version()
+        client.send(message.encode())
+    elif data == "programs":
+        processes = psutil.process_iter()
+        client.send(processes.encode())
+
+    else:
+        message = "Error command: " + data
+        client.send(message.encode())
+
+client.close()
