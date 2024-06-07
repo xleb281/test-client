945d32effaf030a5af596051b101cfb32cca14ef
xleb281 <160697847+xleb281@users.noreply.github.com>
2024-06-07 12:44:01 +0500

    Add files via upload

---
+++ control.py
@@ -0,0 +1,16 @@
+import requests
+
+com = input("command:")
+data1 = input("data1:")
+data2 = input("data2:")
+message = {
+    'sender': 'client',
+    'text': com,
+    'data1': data1,
+    'data2': data2 
+}
+
+response = requests.post('http://127.0.0.1:5000/send_message', json=message)
+
+if response.status_code == 200:
+    print('Message sent successfully!')
